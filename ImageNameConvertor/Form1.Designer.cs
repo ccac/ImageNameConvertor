8159679f00d10256767bb9078a218f4b368c9ae8
AlanChen <alan.chen@morningstar.com>
2013-12-25 10:33:01 +0800

    init commit

---
+++ ImageNameConvertor/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ImageNameConvertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择要转换的文件:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "选择存放目录:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(48, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(308, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(48, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(308, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "D:\\";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(48, 162);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "拍照时间";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "选择转换规则:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(362, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "转换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 204);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(471, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(70, 17);
+            this.toolStripStatusLabel1.Text = "已选中文件:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel2.Text = "0";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 226);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "图片转换器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ ImageNameConvertor/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+
+namespace ImageNameConvertor
+{
+    public partial class Form1 : Form
+    {
+        private List<string> sourceFiles = null;
+        public string folderName { get; set; }
+
+        public string FolderN { get; set; }
+
+        private int counter = 0;
+        private BackgroundWorker worker = null;
+
+        private static Regex r = new Regex(":");
+
+        public Form1()
+        {
+            InitializeComponent();
+            sourceFiles = new List<string>();
+            toolStripStatusLabel2.Text = "0";
+            folderName = "D:\\";
+            //textBox2.DataBindings.Add("Text", this, "FolderN", false, DataSourceUpdateMode.OnPropertyChanged);
+            //textBox2.DataBindings.Add(new Binding("Text", this, "FolderN", false, DataSourceUpdateMode.OnPropertyChanged));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderD = new FolderBrowserDialog();
+            if (folderD.ShowDialog() == DialogResult.OK)
+            {
+                folderName = folderD.SelectedPath;
+                FolderN = folderD.SelectedPath;
+                textBox2.Text = folderName;
+                FolderN = "100";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openD = new OpenFileDialog();
+            openD.Multiselect = true;
+            openD.Filter = "Image Files (.jpg,.bmp,.png)|*.jpg;*.bmp;*.png|All Files|*.*";
+            if (openD.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                sourceFiles.Clear();
+                foreach (string path in openD.FileNames)
+                {
+                    if (".JPG" == Path.GetExtension(path).ToUpper())
+                    {
+                        sourceFiles.Add(path);
+                        sb.Append(Path.GetFileName(path)).Append(";");
+                    }
+                }
+                textBox1.Text = sb.ToString();
+                toolStripStatusLabel2.Text = sourceFiles.Count.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sourceFiles.Count <= 0 || String.IsNullOrEmpty(folderName))
+            {
+                return;
+            }
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+            counter = 0;
+            worker = new BackgroundWorker();
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            toolStripStatusLabel3.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string extension = "";
+            string newfileName = "";
+            Image image = null;
+            foreach (string path in sourceFiles)
+            {
+                counter++;
+                worker.ReportProgress(counter * 100 / sourceFiles.Count);
+                extension = Path.GetExtension(path);
+                newfileName = Path.GetFileNameWithoutExtension(path);
+                try
+                {
+                    /*
+                    image = null;
+                    image = Image.FromFile(path);
+                    if (image.PropertyIdList.Contains<int>(36867))
+                    {
+                        PropertyItem item = image.GetPropertyItem(36867);   //date taken
+                        if (item != null)
+                        {
+                            DateTime dateTaken = DateTime.Parse(r.Replace(Encoding.UTF8.GetString(item.Value), "-", 2));
+                            newfileName = string.Format("{0}{1}{2} {3}{4}{5}",
+                                dateTaken.Year,
+                                dateTaken.Month.ToString("#00"),
+                                dateTaken.Day.ToString("#00"),
+                                dateTaken.Hour.ToString("#00"),
+                                dateTaken.Minute.ToString("#00"),
+                                dateTaken.Second.ToString("#00"));
+                        }
+                    }
+                     */
+                    DateTime dateTaken = GetDateTakenFromImage(path);
+                    if (dateTaken != DateTime.MinValue)
+                    {
+                        newfileName = string.Format("{0}{1}{2} {3}{4}{5}",
+                                                       dateTaken.Year,
+                                                       dateTaken.Month.ToString("#00"),
+                                                       dateTaken.Day.ToString("#00"),
+                                                       dateTaken.Hour.ToString("#00"),
+                                                       dateTaken.Minute.ToString("#00"),
+                                                       dateTaken.Second.ToString("#00"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+                finally
+                {
+                    if (image != null)
+                        image.Dispose();
+                }
+
+                CheckFileName(ref newfileName, extension);
+                File.Copy(path, String.Format("{0}\\{1}{2}", folderName, newfileName, extension), true);
+                System.Threading.Thread.Sleep(1);   //sleep 1 mini-second to update main view.
+            }
+        }
+
+        private void CheckFileName(ref string filename, string extension)
+        {
+            if (File.Exists(String.Format("{0}\\{1}{2}", folderName, filename, extension)))
+            {
+                string format = filename + " ({0})";
+                string tmpName = filename;
+                for (var i = 1; i <= 10; i++)
+                {
+                    tmpName = string.Format(format, i);
+                    if (File.Exists(String.Format("{0}\\{1}{2}", folderName, tmpName, extension)))
+                    {
+                        continue;
+                    }
+                    break;
+                }
+                filename = tmpName;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (worker != null && worker.IsBusy)
+            {
+                DialogResult result = MessageBox.Show("图片正在处理中，确认要退出程序？", "", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    worker.CancelAsync();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        //retrieves the datetime WITHOUT loading the whole image
+        private DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (Image myImage = Image.FromStream(fs, false, false))
+                {
+                    if (myImage.PropertyIdList.Contains<int>(36867))
+                    {
+                        PropertyItem propItem = myImage.GetPropertyItem(36867);
+                        string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                        return DateTime.Parse(dateTaken);
+                    }
+                    return DateTime.MinValue;
+                }
+            }
+        }
+    }
+}
